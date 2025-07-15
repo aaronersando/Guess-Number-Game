9c3a73030a0c685183a2dd5fd5d646705f8b283a
aaronersando <agersando@gmail.com>
2025-07-16 04:27:14 +0800

    Refactor Title component to use Platform.select for border width, improving cross-platform consistency.

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 export default function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -11,7 +11,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 24,
     fontFamily: "open-sans-bold",
-    borderWidth: 2,
+    // borderWidth: 2,
+    // borderWidth: Platform.OS == 'android' ? 2 : 0,
+    borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: "white",
     maxWidth: "80%",
     width: 300,
